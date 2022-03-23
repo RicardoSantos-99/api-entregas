ee575909dfea65044d723fd093a1178993d81789
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2022-03-23 20:06:13 -0300

    config(Global): att script

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,14 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
+	const { name, email } = request.body;
 
-  const user = {
-    name,
-    email,
-  };
+	const user = {
+		name,
+		email,
+	};
 
-  return response.json(user);
+	return response.json(user);
 });
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -11,24 +11,19 @@ app.use(express.json());
 app.use(routes);
 
 app.use((error: Error, _: Request, response: Response) => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
+	if (error instanceof AppError) {
+		return response.status(error.statusCode).json({
+			status: 'error',
+			message: error.message,
+		});
+	}
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
+	return response.status(500).json({
+		status: 'error',
+		message: 'Internal server error',
+	});
 });
 
 app.listen(3333, () => {
-  logger.info('Server started on port 3333');
-  logger.debug('this is a debug message');
-  logger.info('this is a info message');
-  logger.error('this is a error message');
-  logger.warn('this is a warn message');
-  logger.http('this is a http message');
+	logger.info('Server started on port 3333');
 });
